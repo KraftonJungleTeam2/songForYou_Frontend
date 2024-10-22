ccca88332570fd79ea9f2f453a5266e809970218
eggcoffee01 <developerben01@gmail.com>
2024-10-22 15:26:56 +0900

    AuthContext Updated

--- src/components/SongListArea.js
+++ src/components/SongListArea.js
@@ -7,7 +7,7 @@ function SongListArea({ onSongSelect, publicSongs, privateSongs }) {
   const [viewType, setViewType] = useState('private');
   const [searchTerm, setSearchTerm] = useState('');
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  
+
   const handleTabClick = (view) => {
     setViewType(view);
   };
@@ -30,46 +30,26 @@ function SongListArea({ onSongSelect, publicSongs, privateSongs }) {
   };
 
   return (
-    <div className="song-list-area">
-      <div className="top-section">
-        <div className="tabs">
-          <button
-            className={viewType === 'private' ? 'active' : ''}
-            onClick={() => handleTabClick('private')}
-          >
+    <div className='song-list-area'>
+      <div className='top-section'>
+        <div className='tabs'>
+          <button className={viewType === 'private' ? 'active' : ''} onClick={() => handleTabClick('private')}>
             Private
           </button>
-          <button
-            className={viewType === 'public' ? 'active' : ''}
-            onClick={() => handleTabClick('public')}
-          >
+          <button className={viewType === 'public' ? 'active' : ''} onClick={() => handleTabClick('public')}>
             Public
           </button>
         </div>
-        <div className="search-bar">
-          <input 
-            type="text" 
-            placeholder="Hinted search text" 
-            value={searchTerm}
-            onChange={handleSearch}
-          />
+        <div className='search-bar'>
+          <input type='text' placeholder='Hinted search text' value={searchTerm} onChange={handleSearch} />
           <button>🔍</button>
         </div>
-        <button className="add-song-button" onClick={handleOpenPopup}>+ 곡 추가</button>
+        <button className='add-song-button' onClick={handleOpenPopup}>
+          + 곡 추가
+        </button>
       </div>
-      <SongList 
-        viewType={viewType} 
-        searchTerm={searchTerm} 
-        onSongSelect={onSongSelect} 
-        songs={viewType === 'public' ? publicSongs : privateSongs}
-        onOpenPopup={handleOpenPopup}
-      />
-      {isPopupOpen && (
-        <SongUploadPopup
-          onClose={handleClosePopup}
-          onFileUpload={handleFileUpload}
-        />
-      )}
+      <SongList viewType={viewType} searchTerm={searchTerm} onSongSelect={onSongSelect} songs={viewType === 'public' ? publicSongs : privateSongs} onOpenPopup={handleOpenPopup} />
+      {isPopupOpen && <SongUploadPopup onClose={handleClosePopup} onFileUpload={handleFileUpload} />}
     </div>
   );
 }
