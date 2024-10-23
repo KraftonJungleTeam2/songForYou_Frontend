458e2a71d374fd9a6ac9a1309b772711427ae411
eggcoffee01 <developerben01@gmail.com>
2024-10-24 04:20:05 +0900

    3rd demo done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,15 +20,13 @@ function Login() {
         // JWT를 헤더에서 추출
 
         const jwtToken = response.headers['authorization']?.split(' ')[1]; // 'Bearer '를 제거하고 토큰만 가져옴
-
+        console.log('response', response.headers);
         // 사용자 정보는 response.data에 있습니다
         const userData = response.data;
-
         // JWT를 세션 스토리지에 저장
         if (jwtToken) {
           sessionStorage.setItem('userToken', jwtToken);
         }
-
         setError('');
         setIsLoggedIn(true);
         navigate('/single');
