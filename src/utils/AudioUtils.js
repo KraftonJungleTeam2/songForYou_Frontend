936098c020a487e9d0983223daaa457ea33b1412
HwangJunYong <ghkdwnsyong2002@gmail.com>
2024-10-23 19:27:23 +0900

    pichdetection updated

--- src/utils/AudioUtils.js
+++ src/utils/AudioUtils.js
@@ -1,16 +1,22 @@
 export const setupAudioContext = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const analyser = audioContext.createAnalyser();
-    const source = audioContext.createMediaStreamSource(stream);
-    source.connect(analyser);
-    return { audioContext, analyser, source };
-  };
-  
-  export const calculateRMS = (buffer) => {
-    let sum = 0;
-    for (let i = 0; i < buffer.length; i++) {
-      sum += buffer[i] * buffer[i];
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: {
+      echoCancellation: true,
+      noiseSuppression: true,
+      autoGainControl: true
     }
-    return Math.sqrt(sum / buffer.length);
-  };
+  });
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const analyser = audioContext.createAnalyser();
+  const source = audioContext.createMediaStreamSource(stream);
+  source.connect(analyser);
+  return { audioContext, analyser, source };
+};
+
+export const calculateRMS = (buffer) => {
+  let sum = 0;
+  for (let i = 0; i < buffer.length; i++) {
+    sum += buffer[i] * buffer[i];
+  }
+  return Math.sqrt(sum / buffer.length);
+};
