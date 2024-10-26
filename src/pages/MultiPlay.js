6695d09fd3a4bd747ca196cb89e728a4a7a62166
ChoongGhee <tititito44@gmail.com>
2024-10-26 18:59:45 +0900

    create multiplay

---
+++ src/pages/MultiPlay.js
@@ -0,0 +1,13 @@
+import TopBar from "../components/TopBar";
+
+
+function MultiPlay() {
+
+    return(
+        <div className="multi-page">
+            <TopBar></TopBar>
+            <h>Hello</h>
+        </div>
+    );
+}
+export default MultiPlay
