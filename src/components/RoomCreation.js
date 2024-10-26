4f1a25e14a622c234703463cf764d67621dde68d
ChoongGhee <tititito44@gmail.com>
2024-10-26 16:54:47 +0900

    room create API OK

--- src/components/RoomCreation.js
+++ src/components/RoomCreation.js
@@ -1,25 +1,78 @@
 // src/components/RoomCreation.js
-import React from 'react';
-import '../css/RoomCreation.css'; // 필요 시 스타일 파일 추가
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../css/RoomCreation.css';
 
 function RoomCreation({ onCancel }) {
+  const [roomTitle, setRoomTitle] = useState('');
+  const [roomPassword, setRoomPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //토큰 가져오기
+    const token = sessionStorage.getItem('userToken');
+      if (!token) {
+        alert('No JWT token found');
+        console.error('No JWT token found');
+        onCancel(); // 토큰이 없을 경우 onCancel 호출
+        return; // 함수 실행 중단
+      }
+
+    try {
+      const response = await axios.post(
+        '/api/rooms/create',
+        { roomTitle, roomPassword }, // data
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`, // 가져온 토큰을 헤더에 추가
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        const { roomId } = response.data;
+        alert(`Room created successfully! Room ID: ${roomId}`);
+        
+        // 해당 방으로 이동
+      } else {
+        alert('Failed to create room');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error creating room');
+    }
+  };
+
   return (
     <div className='room-creation-background'>
-        <div className="room-creation-window">
+      <div className="room-creation-window">
         <h2>방 생성</h2>
-        <form>
-            <label>
+        <form onSubmit={handleSubmit}>
+          <label>
             방 제목:
-            <input type="text" name="roomTitle" required />
-            </label>
-            <label>
+            <input
+              type="text"
+              name="roomTitle"
+              value={roomTitle}
+              onChange={(e) => setRoomTitle(e.target.value)}
+              required
+            />
+          </label>
+          <label>
             비밀번호:
-            <input type="password" name="roomPassword" required />
-            </label>
-            <button type="submit">생성</button>
-            <button type="button" onClick={onCancel}>취소</button>
+            <input
+              type="text"
+              name="roomPassword"
+              value={roomPassword}
+              onChange={(e) => setRoomPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">생성</button>
+          <button type="button" onClick={onCancel}>취소</button>
         </form>
-        </div>
+      </div>
     </div>
   );
 }
