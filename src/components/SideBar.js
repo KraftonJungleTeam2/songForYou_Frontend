cf10a99dd21740e94ccd2cd80f605a42d4209ce8
ChoongGhee <tititito44@gmail.com>
2024-10-27 18:48:12 +0900

    songlist img size modify

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-// src/components/SideBar.js
+// src/components/Sidebar.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/SideBar.css';
@@ -7,13 +7,23 @@ function Sidebar({ isOpen, toggleSidebar }) {
   const navigate = useNavigate();
 
   return (
-    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-      <nav className='sidebar-nav'>
-        <button onClick={() => navigate('/single')}>Single</button>
-        <button onClick={() => navigate('/multi')}>Multi</button>
-        <button onClick={() => navigate('/add')}>Add</button>
-      </nav>
-    </div>
+    <>
+      <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+        <nav className='sidebar-nav'>
+          <button onClick={() => navigate('/single')}>Single</button>
+          <button onClick={() => navigate('/multi')}>Multi</button>
+          <button onClick={() => navigate('/add')}>Add</button>
+        </nav>
+      </div>
+
+      {/* 사이드바 열기/닫기 버튼을 화면에 고정 */}
+      <button
+        onClick={toggleSidebar}
+        className={`toggle-button ${isOpen ? 'open' : 'closed'}`}
+      >
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+    </>
   );
 }
 
