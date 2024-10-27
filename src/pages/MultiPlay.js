7f2b9dd8be40cd7109977ef27775acf9d2729382
ChoongGhee <tititito44@gmail.com>
2024-10-27 22:59:02 +0900

    testgood

--- src/pages/MultiPlay.js
+++ src/pages/MultiPlay.js
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import TopBar from "../components/TopBar";
-import '../css/MultiPlay.css'
-
+import '../css/MultiPlay.css';
+import AudioPlayer from "../components/AudioPlayer";
 
 function MultiPlay() {
     const [players, setPlayers] = useState(Array(8).fill(null)); // 8자리 초기화
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [userSeekPosition, setUserSeekPosition] = useState(0);
+    const [audioLoaded, setAudioLoaded] = useState(false);
+    const [duration, setDuration] = useState(0);
 
-    // setPlayers(updatedPlayers.slice(0, 8)); // 항상 8개의 자리 유지
+    const handlePlaybackPositionChange = (position) => {
+        setUserSeekPosition(position);
+    };
+
     return (
         <div className="multiPlay-page">
             <TopBar className="top-bar" />
@@ -14,9 +21,18 @@ function MultiPlay() {
                 <div className="players">
                     {players.map((player, index) => (
                         <div key={index} className="player-card">
-                            {player ? <p>{player.name}</p> : <p>빈 자리</p>}
+                            {player ? (
+                                <div>
+                                    <p>{player.name}</p>
+                                </div>
+                            ) : (
+                                <p>빈 자리</p>
+                            )}
                         </div>
                     ))}
+                </div>
+                <div>
+                  <AudioPlayer isPlaying={isPlaying} setIsPlaying={setIsPlaying} userSeekPosition={userSeekPosition} audioBlob={player.audioBlob} setAudioLoaded={setAudioLoaded}  setDuration={setDuration}  onPlaybackPositionChange={handlePlaybackPositionChange}/>
                 </div>
             </div>
         </div>
