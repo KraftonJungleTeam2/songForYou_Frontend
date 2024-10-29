fde6fe3033e8fdc4e186cea4c0fffb41234c7311
ChoongGhee <tititito44@gmail.com>
2024-10-29 19:45:28 +0900

    나뭐했지

--- src/components/ReservationPopup.js
+++ src/components/ReservationPopup.js
@@ -1,27 +1,115 @@
-import { useEffect, useState, useRef } from 'react';
-import ReservationList from './ReservationList';
+import React, { useEffect, useState } from 'react';
 import { useSongs } from '../Context/SongContext';
 
-const ReservationPopup = ({ onClose }) => {
+const ReservationPopup = ({ soket, onClose }) => {
+  const { songLists, fetchSongLists } = useSongs();
+  const [viewType, setViewType] = useState('public');
+  const [searchTerm, setSearchTerm] = useState('');
 
-    const [selectedSong, setSelectedSong] = useState(null);
-    const { songLists, fetchSongLists } = useSongs();
-    
-    // songContext에서 노래 정보를 불러옴
-    useEffect(() => {
-        fetchSongLists();
-    }, []);
+  // songContext에서 노래 정보를 불러옴
+  useEffect(() => {
+    fetchSongLists();
+  }, [fetchSongLists]);
 
-    const handleSongSelect = (song) => {
-        setSelectedSong(song);
-      };
+  const handleTabClick = (view) => {
+    setViewType(view);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleReserve = (e, song) => {
+    e.stopPropagation();
+    // 곡 예약 정보 소켓으로 전달하기
+
+    onClose();
+  };
+
+  function arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  }
+
+  const filteredSongs = (viewType === 'public' ? songLists.public : songLists.private).filter((song) =>
+    song.metadata.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div style={styles.overlay}>
-      <div style={styles.popup}>
-        <ReservationList onSongSelect={handleSongSelect} publicSongs={songLists.public} privateSongs={songLists.private}></ReservationList>
-        <button className='button' onClick={onClose}>닫기</button>
-      </div>
+        <div style={styles.popup}>
+            <div className='song-list-area'>
+                <div className='top-section'>
+                
+                
+                
+                    <div className='tabs' style={{ paddingBottom: '1px', margin: '0rem auto 0rem 0rem' }}>
+                    <li className={viewType === 'public' ? 'is-active' : ''} onClick={() => handleTabClick('public')}>
+                        <a>공개 노래</a>
+                    </li>
+                    <li className={viewType === 'private' ? 'is-active' : ''} onClick={() => handleTabClick('private')}>
+                        <a>내 노래</a>
+                    </li>
+                    </div>
+
+
+
+                    <div className='search-bar'>
+                    <input type='text' placeholder='노래 검색' value={searchTerm} onChange={handleSearch} />
+                    <button className='button is-dark'>
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                    </div>
+
+                </div>
+            
+                <div className='song-list'>
+                    {filteredSongs.length === 0 ? (
+                    <p>노래가 없습니다.</p>
+                    ) : (
+                    filteredSongs.map((song) => (
+                        <div key={song.id} className='song-item'>
+                            <div
+                                className='song-icon'
+                                style={{
+                                backgroundImage: `url(data:image/jpeg;base64,${arrayBufferToBase64(song.image.data)})`,
+                                backgroundSize: 'cover',
+                                backgroundRepeat: 'no-repeat',
+                                width: '4rem',
+                                height: '4rem',
+                                }}
+                                alt={song.metadata.title}
+                            />
+
+                        <div className='song-info'>
+                            <h3>{song.metadata.title}</h3>
+                            <p>{song.metadata.description}</p>
+                            <span className='timestamp'>{song.timestamp}</span>
+                        </div>
+                            <div 
+                                className='play-button has-text-success' 
+                                style={{ cursor: 'pointer' }} 
+                                onClick={(e) => handleReserve(e, song)}
+                                >
+                                <i className="fa-solid fa-plus"></i> {/* 플러스 아이콘 */}
+                            </div>
+
+                        </div>
+                    ))
+                    )}
+                </div>
+
+                
+            </div>
+
+
+            <button className='button' onClick={onClose}>닫기</button>
+        </div>
     </div>
   );
 };
@@ -43,7 +131,7 @@ const styles = {
     backgroundColor: '#fff',
     padding: '20px',
     borderRadius: '8px',
-    width: '50rem',
+    width: '40rem',
     height: '80%',
     textAlign: 'center',
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
