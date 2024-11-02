2389350aaba1d4746762ec3d9c618e8cae5d84b8
HwangJunYong <ghkdwnsyong2002@gmail.com>
2024-11-03 01:14:12 +0900

    pitchData messaging DONE

--- src/utils/AudioUtils.js
+++ src/utils/AudioUtils.js
@@ -1,11 +1,4 @@
-export const setupAudioContext = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: {
-      echoCancellation: true,
-      noiseSuppression: true,
-      autoGainControl: false,
-    },
-  });
+export const setupAudioContext = async (stream) => {
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
   const analyser = audioContext.createAnalyser();
   const source = audioContext.createMediaStreamSource(stream);
