ef9077b621049f5eae8be78fe96d45abcc8e2189
ChoongGhee <tititito44@gmail.com>
2024-11-05 00:32:10 +0900

    single work3

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -120,7 +120,7 @@ function Preview({ selectedSong }) {
       
       <div className={isMobile ? 'icon1' : 'icon'}>{selectedSong.image && selectedSong.image.data ? <img src={`data:image/jpeg;base64,${arrayBufferToBase64(selectedSong.image.data)}`} alt={selectedSong.metadata.title} /> : <div>No Image Available</div>}</div>
      
-      <div className='info'>
+      <div className={isMobile ? 'info1' : 'info'}>
         <h3 className='title'>{selectedSong.metadata.title}</h3>
         
         <p className='subtitle'>{selectedSong.metadata.description}</p>
