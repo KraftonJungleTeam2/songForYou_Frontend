f6a301debf5a436d51c8ce0eac5061ecc58dd12c
ChoongGhee <tititito44@gmail.com>
2024-11-06 00:39:41 +0900

    schem

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -314,7 +314,7 @@ const Play = () => {
             </button>
             <input type='range' min='0' max={duration} step='0.025' value={playbackPosition} onChange={handlePlaybackPositionChange} className='range-slider' disabled={!dataLoaded} />
             <div className='playback-info'>
-              {playbackPosition.toFixed(3)} / {Math.floor(duration)} 초
+              {playbackPosition.toFixed(1)} / {Math.floor(duration)} 초
             </div>
 
             {!dataLoaded && <p className='loading-text'>데이터 로딩 중...</p>}
