56094fe8f47c699990e055bf95db39941b8921db
json-yun <jsyun833@gmail.com>
2024-11-07 01:39:24 +0900

    className

--- src/components/DarkButton.js
+++ src/components/DarkButton.js
@@ -30,7 +30,7 @@ const DarkModeToggle = () => {
 
   return (
     <button style={buttonStyle} className="dark-mode-toggle" onClick={toggleTheme}>
-      {theme === 'dark' ? <i class="fa-solid fa-sun"></i> : <i class="fa-solid fa-moon"></i>}
+      {theme === 'dark' ? <i className="fa-solid fa-sun"></i> : <i className="fa-solid fa-moon"></i>}
     </button>
   );
 };
