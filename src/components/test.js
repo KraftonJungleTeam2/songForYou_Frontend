e7c303882c563ae8d0a4830c2fb9892376795485
ChoongGhee <tititito44@gmail.com>
2024-11-07 03:32:00 +0900

    page Template create

---
+++ src/components/test.js
@@ -0,0 +1,12 @@
+<div className='single-page'>
+    {isMobile ? (
+    <MobileNav />
+    ) : (
+    <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+    )}
+    <div className={`main-content ${isSidebarOpen ? 'shifted' : ''}`}>
+        <TopBar />
+        <div className='content-area'></div>
+        </div>
+    </div>
+</div>
