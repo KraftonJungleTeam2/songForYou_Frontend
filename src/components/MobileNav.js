cd99491c5c33dad0be4dad49808a20dec936360f
json-yun <jsyun833@gmail.com>
2024-11-07 20:37:13 +0900

    .

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -19,18 +19,21 @@ function MobileNav() {
                 onClick={() => navigate('/single')}
             >
               <i className="fa-solid fa-user"></i>
+              <p className="nav-caption">혼자서</p>
             </div>
             <div
                 className={`nav-button-mobile ${isActive('/add') ? 'active' : ''}`}
                 onClick={() => navigate('/add')}
             >
               <i className="fa-solid fa-circle-plus" style={{fontSize:'2.5rem'}}></i>
+              <p className="nav-caption">다같이</p>
             </div>
             <div
                 className={`nav-button-mobile ${isActive('/multi') ? 'active' : ''}`}
                 onClick={() => navigate('/multi')}
             >
               <i className="fa-solid fa-users"></i>
+              <p className="nav-caption">노래 추가</p>
             </div>
         </div>
     );
