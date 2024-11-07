49f75f82b492ef93b21bfd6fc9ac335348c1027b
ChoongGhee <tititito44@gmail.com>
2024-11-07 16:16:04 +0900

    template multi

--- src/pages/Multi.js
+++ src/pages/Multi.js
@@ -5,9 +5,9 @@ import Sidebar from '../components/SideBar';
 import RoomCreation from '../components/RoomCreation';
 import { useNavigate } from 'react-router-dom';
 import { useScreen } from '../Context/ScreenContext';
-import SimpleSideBar from '../components/SimpleSideBar';
 import '../css/Multi.css';
 import MobileNav from '../components/MobileNav';
+import PageTemplate from '../template/PageTemplate';
 
 function Multi() {
   const [rooms, setRooms] = useState([]);
@@ -85,60 +85,50 @@ function Multi() {
   }
 
   return (
-    <div className='single-page'>
-      {isMobile ? (
-        <MobileNav/>
+    <PageTemplate isMobile={isMobile} isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} >
+      {isCreatingRoom ? (
+        <RoomCreation onCancel={() => setIsCreatingRoom(false)} />
       ) : (
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      )}
-      <div className={`multi-main ${isSidebarOpen ? 'shifted' : ''}`}>
-        <TopBar />
-        <div className='content-area-multi'>
-          {isCreatingRoom ? (
-            <RoomCreation onCancel={() => setIsCreatingRoom(false)} />
-          ) : (
-            <div className="main-area">
-              <button className="page-button" onClick={handlePreviousPage}>
-                <i className="fa-solid fa-circle-chevron-left"></i>
-              </button>
+        <div className="main-area">
+          <button className="page-button" onClick={handlePreviousPage}>
+            <i className="fa-solid fa-circle-chevron-left"></i>
+          </button>
 
-              <div className="room-list">
-                {roomCards.map((room, index) => (
-                  <div className={`room-card ${room.empty ? 'empty' : ''}`} key={room.empty ? `empty-${index}` : room.id}>
-                    {room.empty ? (
-                      <div className="room-info-empty">빈 방</div>
-                    ) : (
-                      <div onClick={(e) => handlePlay(e, room.id, room.password)} className="room-content">
-                        <img className="thumbnail" src={room.image} alt={`Thumbnail for ${room.roomTitle}`} />
-                        <div className="room-info">
-                          <h3>{room.roomTitle}</h3>
-                          <p>Players: {room.users.length}/{room.max_user}</p>
-                        </div>
-                      </div>
-                    )}
+          <div className="room-list">
+            {roomCards.map((room, index) => (
+              <div className={`room-card ${room.empty ? 'empty' : ''}`} key={room.empty ? `empty-${index}` : room.id}>
+                {room.empty ? (
+                  <div className="room-info-empty">빈 방</div>
+                ) : (
+                  <div onClick={(e) => handlePlay(e, room.id, room.password)} className="room-content">
+                    <img className="thumbnail" src={room.image} alt={`Thumbnail for ${room.roomTitle}`} />
+                    <div className="room-info">
+                      <h3>{room.roomTitle}</h3>
+                      <p>Players: {room.users.length}/{room.max_user}</p>
+                    </div>
                   </div>
-                ))}
+                )}
               </div>
-
-              <button className="page-button" onClick={handleNextPage}>
-                <i className="fa-solid fa-circle-chevron-right"></i>
-              </button>
-            </div>
-          )}
-
-          <div className={`bottom-buttons ${isSidebarOpen ? 'shifted' : ''}`}>
-            <button className='quick-join'>빠른 입장</button>
-            <button className='sort-by'>정렬 조건</button>
-            <button className='create-room' onClick={() => setIsCreatingRoom(true)}>
-              방 생성
-            </button>
-            <button className='refresh' onClick={fetchRooms}>
-              새로고침
-            </button>
+            ))}
           </div>
+
+          <button className="page-button" onClick={handleNextPage}>
+            <i className="fa-solid fa-circle-chevron-right"></i>
+          </button>
         </div>
+      )}
+
+      <div className={`bottom-buttons ${isSidebarOpen ? 'shifted' : ''}`}>
+        <button className='quick-join'>빠른 입장</button>
+        <button className='sort-by'>정렬 조건</button>
+        <button className='create-room' onClick={() => setIsCreatingRoom(true)}>
+          방 생성
+        </button>
+        <button className='refresh' onClick={fetchRooms}>
+          새로고침
+        </button>
       </div>
-    </div>
+    </PageTemplate>
   );
 }
 
