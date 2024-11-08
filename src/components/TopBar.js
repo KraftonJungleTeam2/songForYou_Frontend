c3d51298867bcfd4521fd25bb47102c7453e85e9
json-yun <jsyun833@gmail.com>
2024-11-09 00:03:10 +0900

    kill logout

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -45,31 +45,6 @@ function TopBar() {
     }
   };
 
-  const Logout = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      const token = sessionStorage.getItem("userToken");
-      const response = await axios.delete(
-        `${process.env.REACT_APP_API_ENDPOINT}/users/logout`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      // 세션 스토리지에서 토큰과 사용자 정보 삭제
-      sessionStorage.removeItem("userToken");
-
-      setIsLoggedIn(false);
-      // 로그아웃 성공 시 로그인 페이지로 이동
-      navigate("/login");
-    } catch (err) {
-      alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
-      console.error("Logout error", err);
-    }
-  };
 
   const GoSetting = () => {
     navigate("/setting");
@@ -80,16 +55,13 @@ function TopBar() {
       {isMobile ? <DarkModeToggle /> : ""}
 
       <div className="right-section">
-        <button className="button is-dark logout-button" onClick={Logout}>
-          로그아웃
-        </button>
         <div className="user-avatar">A</div>
         <div className="user-details">
           <h3>{userData.name}</h3>
           <p>{userData.email}</p>
         </div>
         <button className="settings-button" onClick={GoSetting}>
-          ⚙️
+        <i className="fa-solid fa-gear"></i>
         </button>
       </div>
     </div>
