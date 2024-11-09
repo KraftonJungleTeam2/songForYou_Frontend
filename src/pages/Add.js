f9c251740fa6327ed35dae816babe57c5dc82fde
json-yun <jsyun833@gmail.com>
2024-11-09 21:21:42 +0900

    merge

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -303,8 +303,7 @@ function Add() {
                 </label>
               </div>
               <button
-                className={`button is-highlight ${isWaiting ? "is-loading" : ""}`}
-                className={`add-button ${isWaiting ? "is-loading" : ""}`}
+                className={`add-button is-highlight ${isWaiting ? "is-loading" : ""}`}
                 disabled={isWaiting}
                 type="submit"
                 style={{marginTop: '2em'}}
